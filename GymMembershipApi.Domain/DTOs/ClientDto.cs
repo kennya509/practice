6c4d0915c5c02d16eb6531ee89fe05d6be567b62
kennya <verbitskyi_nikita@chnu.edu.ua>
2025-07-02 18:25:16 +0300

    docs: Add Technical Task and Report files

---
+++ GymMembershipApi.Domain/DTOs/ClientDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GymMembershipApi.Domain.DTOs
+{
+    public class ClientDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
---
+++ GymMembershipApi.Domain/DTOs/CreateClientDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GymMembershipApi.Domain.DTOs
+{
+    public class CreateClientDto
+    {
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
