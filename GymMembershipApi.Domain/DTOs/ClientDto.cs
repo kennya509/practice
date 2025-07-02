c11368c73819db778a826e9bbea2655c961f0dbf
kennya <verbitskyi_nikita@chnu.edu.ua>
2025-07-02 18:30:11 +0300

    chore: Final

--- GymMembershipApi.Domain/DTOs/ClientDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GymMembershipApi.Domain.DTOs
-{
-    public class ClientDto
-    {
-        public int Id { get; set; }
-        public string FullName { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-    }
-}
